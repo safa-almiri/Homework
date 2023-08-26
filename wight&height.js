25aefe812f04a4fc1deccea2a8c555af8609f783
safa-almiri <142091455+safa-almiri@users.noreply.github.com>
2023-08-26 22:10:24 +0300

    Add files via upload

---
+++ wight&height.js
@@ -0,0 +1,26 @@
+
+w=prompt("enter your wight")
+h=prompt("enter your height")
+
+a=(w*10)/((h*h)*.001)
+alert(a)
+
+if (a <= 17){
+    alert("magaah")
+}    
+else if (a >17 && a <= 25 ){
+ alert("normal")   
+}
+    
+else if (a >25 && a <= 30 ){
+ alert("banda")   
+}
+    
+else if (a > 35 && a <= 40 ){
+ alert("Dangerous")   
+}
+
+
+
+
+
